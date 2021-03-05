b98d56cf46f0d754619aa2de2bc78f60cab9bbc2
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2021-03-05 11:27:46 -0800

    updated comments

--- JavaMultiThreading/src/com/chandu/multithreading/Interthreadcom/InterThreadMain.java
+++ JavaMultiThreading/src/com/chandu/multithreading/Interthreadcom/InterThreadMain.java
@@ -6,24 +6,25 @@ public class InterThreadMain {
 
 		// Course object on which wait and notify method will be called.
 		Course course = new Course("Java");
+
+		// StudentWait threads which will wait for completion of Course
 		StudentWait st1 = new StudentWait(course);
 		StudentWait st2 = new StudentWait(course);
 
-		// StudentWait threads which will wait for completion of book
 		Thread th1 = new Thread(st1, "Thread1");
 		Thread th2 = new Thread(st2, "Thread2");
 
 		th1.start();
 		th2.start();
 
-		// To ensure both students started waiting for the book
+		// To ensure both students started waiting for the course
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 
 			e.printStackTrace();
 		}
-		// StudentNotify thread which will notify once book get completed
+		// StudentNotify thread which will notify once course get completed
 		StudentNotify stn = new StudentNotify(course);
 		Thread notifyth = new Thread(stn);
 		notifyth.start();
